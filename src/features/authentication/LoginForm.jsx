4748011ebdde3de687a569f37b762e2f965b930f
Geoffrey <acgeoffrey@users.noreply.github.com>
2023-08-23 16:37:18 +0530

    add: show error in invalid login details

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import FormElement from '../../ui/FormElement';
 import LoaderMini from '../../ui/LoaderMini';
@@ -7,11 +7,39 @@ import { useLogin } from './useLogin';
 function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState({ email: '', password: '' });
   const { login, isLoading } = useLogin();
+
+  useEffect(
+    function () {
+      if (error.email === 'Email field is required') {
+        if (email.length > 0) setError((err) => ({ ...err, email: '' }));
+        else setError((err) => ({ ...err, email: 'Email field is required' }));
+      }
+
+      if (error.password === 'Password must contain minimum of 8 characters') {
+        if (password.length > 7) setError((err) => ({ ...err, password: '' }));
+        else
+          setError((err) => ({
+            ...err,
+            password: 'Password must contain minimum of 8 characters',
+          }));
+      }
+    },
+    [error.password, password.length, error.email, email.length],
+  );
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (!email || !password) return;
+    setError({ email: '', password: '' });
+    if (!email || !password) {
+      setError((err) => ({ ...err, email: 'Email field is required' }));
+      setError((err) => ({
+        ...err,
+        password: 'Password must contain minimum of 8 characters',
+      }));
+      return;
+    }
 
     login(
       { email, password },
@@ -29,7 +57,7 @@ function LoginForm() {
       onSubmit={handleSubmit}
       className='overflow-hidden rounded-xl border-[1px] border-solid border-gray-100 bg-white px-10 py-6 text-base'
     >
-      <FormElement label='Email'>
+      <FormElement label='Email' error={error.email}>
         <input
           type='email'
           id='email'
@@ -38,20 +66,22 @@ function LoginForm() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           disabled={isLoading}
-          className='input'
+          className={`input ${error.email.length > 0 ? ' border-red-600' : ''}`}
         />
       </FormElement>
 
-      <FormElement label='Password'>
+      <FormElement label='Password' error={error.password}>
         <input
           type='password'
           id='password'
-          placeholder='Min. 8 characters'
+          placeholder='password'
           autoComplete='current-password'
           value={password}
           disabled={isLoading}
           onChange={(e) => setPassword(e.target.value)}
-          className='input'
+          className={`input ${
+            error.password.length > 0 ? 'border-red-600' : ''
+          }`}
         />
       </FormElement>
 
--- src/ui/FormElement.jsx
+++ src/ui/FormElement.jsx
@@ -1,14 +1,14 @@
 function FormElement({ label, error, children }) {
   // console.log(children.props);
   return (
-    <div className='flex flex-col gap-3 py-3'>
+    <div className='flex flex-col py-3'>
       {label && (
-        <label htmlFor={children.props.id} className='font-medium'>
+        <label htmlFor={children.props.id} className='mb-3 font-medium'>
           {label}
         </label>
       )}
       {children}
-      {error && <span className='text-xl text-red-700'>{error}</span>}
+      {error && <span className='text-sm text-red-700'>{error}</span>}
     </div>
   );
 }
